991c6d4a4a2132da9075640a4f2d691295cf7511
Zhuying <zhuying0529@163.com>
2015-04-09 21:33:26 +0800

    cv

--- works/pagepiling/js/index.js
+++ works/pagepiling/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#pagepiling').pagepiling({
-    	sectionsColor: ['#E4A3AE', '#2A3D4E', '#222327', '#33B6A0', '#078571', '#C72F7F'],
+    	sectionsColor: ['#E4A3AE', '#2A3D4E', '#222327', '#33B6A0', '#078571','#222327', '#C72F7F'],
     });
 });
