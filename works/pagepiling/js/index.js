add520c3dd9062e9b28389890c98da9b98a722a4
Zhuying <zhuying0529@163.com>
2015-04-10 14:30:56 +0800

    cv finish

--- works/pagepiling/js/index.js
+++ works/pagepiling/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#pagepiling').pagepiling({
-    	sectionsColor: ['#E4A3AE', '#2A3D4E', '#222327', '#33B6A0', '#078571','#222327', '#C72F7F'],
+    	sectionsColor: ['#a54358', '#33B6A0', '#33B6A0','#31A061','#078571','#90caaf','#90caaf','#90caaf','#bed2b6','#C72F7F',],
     });
 });
+// #2A3D4E #33B6A0
