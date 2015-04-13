e0cf068023b0ceb7ac27de7db9cacf955ff23d9d
Zhuying <zhuying0529@163.com>
2015-04-13 15:36:58 +0800

    cv change

--- works/pagepiling/js/index.js
+++ works/pagepiling/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#pagepiling').pagepiling({
-    	sectionsColor: ['#a54358', '#33B6A0', '#33B6A0','#31A061','#078571','#90caaf','#90caaf','#90caaf','#bed2b6','#C72F7F',],
+    	sectionsColor: ['#33B6A0', '#33B6A0', '#33B6A0','#31A061','#078571','#90caaf','#90caaf','#90caaf','#bed2b6','#C72F7F',],
     });
 });
 // #2A3D4E #33B6A0
